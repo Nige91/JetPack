e6016d89a2187b315c405e2de7ae4fc07626faab
Nigel91 <42963496+Nige91@users.noreply.github.com>
2021-01-13 00:40:46 +0100

    Refactoring

--- JetPack/JetPack/Movement/MovementModule.cs
+++ JetPack/JetPack/Movement/MovementModule.cs
@@ -7,7 +7,6 @@ using SkiaSharp.Views.Forms;
 namespace JetPack.Movement
 {
 	//TODO overthink MovementModule class structure
-	[Serializable()]
 	class MovementModule
 	{
 		public SKPoint coords { get; private set; }
@@ -75,72 +74,6 @@ namespace JetPack.Movement
 			{
 				unit.RecalibratePhase(speed);
 			}
-		}
-	}
-
-	class MovementModuleUnit
-	{
-		public SKPoint distance;
-		public float ampMin { get; private set; }
-		public float ampMax { get; private set; }
-		public int phaseDuration { get; private set; }
-		public long phaseStartTime { get; private set; }
-
-		public MovementModuleUnit()
-		{
-		}
-
-		public MovementModuleUnit(SKPoint distance)
-		{
-			this.distance = distance;
-			this.phaseDuration = 0;
-			this.phaseStartTime = 0;
-		}
-
-		public MovementModuleUnit(SKPoint distance, float ampMin, float ampMax, int phaseDuration)
-		{
-			this.distance = distance;
-			this.ampMin = ampMin;
-			this.ampMax = ampMax;
-			this.phaseDuration = phaseDuration;
-			this.phaseStartTime = Helper.GetMilliseconds();
-		}
-
-		public MovementModuleUnit Copy()
-		{
-			MovementModuleUnit unit = new MovementModuleUnit();
-			unit.distance = new SKPoint(this.distance.X, this.distance.Y);
-			unit.ampMin = this.ampMin;
-			unit.ampMax = this.ampMax;
-			unit.phaseDuration = this.phaseDuration;
-			unit.phaseStartTime = this.phaseStartTime;
-			return unit;
-		}
-
-		public SKPoint Move(float speed)
-		{
-			SKPoint finalDistance = new SKPoint();
-			finalDistance.X = distance.X * GetAmplitude() * GetLoopTime();
-			finalDistance.Y = distance.Y * GetAmplitude() * GetLoopTime();
-			return finalDistance;
-		}
-
-		//TODO RecalibratePhaseStart
-		public void RecalibratePhase(float speed)
-		{
-
-		}
-
-		//TODO implement GetAmplitude
-		private float GetAmplitude()
-		{
-			return 1;
-		}
-
-		//TODO implement GetLoopTime
-		private float GetLoopTime()
-		{
-			return (float)1000 / ((float)Globals.fps * (float)Globals.normalTimeUnitInMs);
 		}
 	}
 }
